2e16e47a7a902d124b2960d621aa9d21c03a2ea3
Hunaid <61511895+Hunaid-Memon@users.noreply.github.com>
2021-09-19 00:34:47 +0500

    Update State
    Created state using constructor in class method

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Navbar from './components/layouts/Navbar';
+import UserItem from './components/Users/UserItem';
 import './App.css';
 
 
 class App extends React.Component {
-  
-
   render() {   
     return (
       <div className='App'>
           <Navbar />
+          <UserItem />
       </div>
     )
   }
