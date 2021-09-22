3752959511e77a9dbe36cbc711a059bbbf6c82d6
Hunaid <61511895+Hunaid-Memon@users.noreply.github.com>
2021-09-22 23:29:44 +0500

    Added Search Component
    Added Search Component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
-import React from 'react';
-import Navbar from './components/layouts/Navbar';
-import Users from './components/Users/Users';
-import axios from 'axios';
-import './App.css';
-
+import React from "react";
+import Navbar from "./components/layouts/Navbar";
+import Users from "./components/Users/Users";
+import axios from "axios";
+import "./App.css";
+import Search from "./components/Users/Search";
 
 class App extends React.Component {
   state = {
     users: [],
-    loading: false
-  }
+    loading: false,
+  };
 
-  async componentDidMount() {
-    this.setState({loading: true})
-    const res = await axios.get('https://api.github.com/users')
-    console.log(res.data);
-    this.setState({users: res.data, loading: false})  
-  }
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+  //   // console.log(res.data);
+  //   this.setState({ users: res.data, loading: false });
+  // }
 
-  render() {   
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    // console.log(res.data);
+    this.setState({ users: res.data.items, loading: false });
+  
+  };
+
+  render() {
     return (
       <div className='App'>
-          <Navbar />
-          <div className='container'>
-            <Users loading={this.state.loading} users={this.state.users}/>
-          </div>
+        <Navbar />
+        <div className='container'>
+          <Search searchUsers={this.searchUsers} />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
-    )
+    );
   }
 }
-
 
 export default App;
