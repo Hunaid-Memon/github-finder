86d9b145ff5a7da3645945b579d598011f2e52f7
Hunaid <61511895+Hunaid-Memon@users.noreply.github.com>
2021-10-03 02:35:30 +0500

    Added repos and repoitem component

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -3,22 +3,26 @@ import Spinner from '../layouts/Spinner';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Fragment } from 'react';
+import Repos from '../repos/Repos';
 
 export class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login)
+        this.props.getUserRepos(this.props.match.params.login)
     }
 
     static propTypes = {
         getUser: PropTypes.func.isRequired,
+        getUserRepos: PropTypes.func.isRequired,
         user: PropTypes.object.isRequired,
+        repos: PropTypes.array.isRequired,
         loading: PropTypes.bool.isRequired,
     }
 
     render() {
         const { name, company, avatar_url, location, bio, blog, login, html_url, followers, following, public_repos, public_gists, hireable } = this.props.user;
         
-        const {loading} = this.props;
+        const {loading, repos} = this.props;
 
         if (loading) return <Spinner />
 
@@ -72,6 +76,7 @@ export class User extends Component {
                     <div className="badge badge-light">Public Repos: {public_repos} </div>
                     <div className="badge badge-dark">Public Gists: {public_gists} </div>
                 </div>
+                <Repos repos={repos} />
             </Fragment>
         )
     }
