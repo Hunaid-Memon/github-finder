07fd5fbc21ca348deb83b2cea8d895c18c19b1f2
Hunaid <61511895+Hunaid-Memon@users.noreply.github.com>
2021-10-05 21:15:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
   const searchUsers = async (text) => {
     setLoading(true);
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    setUsers(res.data.item);
+    setUsers(res.data.items);
     setLoading(false);
   };
 
@@ -32,7 +32,7 @@ const App = () => {
   }
 
   const getUserRepos = async (username) => {
-    this.setState({ loading: true });
+    setLoading(true);
     const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     setRepos(res.data);
     setLoading(false);
